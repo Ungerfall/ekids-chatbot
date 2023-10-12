fedf8de98294ff3da1e02e914d59a72d0271683f
Christine_Grace <christine_grace@epam.com>
2023-10-12 09:54:08 +0800

    Removed EnableScheduling

--- src/homework-management-backend/src/main/java/org/braidsencurls/ekids_homework_management/HomeworkManagement.java
+++ src/homework-management-backend/src/main/java/org/braidsencurls/ekids_homework_management/HomeworkManagement.java
@@ -2,9 +2,7 @@ package org.braidsencurls.ekids_homework_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class HomeworkManagement {
     public static void main(String[] args) {
--- src/homework-management-backend/src/main/java/org/braidsencurls/ekids_homework_management/security/AuthenticationService.java
+++ src/homework-management-backend/src/main/java/org/braidsencurls/ekids_homework_management/security/AuthenticationService.java
@@ -3,7 +3,6 @@ package org.braidsencurls.ekids_homework_management.security;
 public interface AuthenticationService {
 
     UserDTO register(UserDTO user);
-
     AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest);
 
 }
