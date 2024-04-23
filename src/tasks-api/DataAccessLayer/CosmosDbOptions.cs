f3cd23c01c8d84a82b9a146533e6c963cbc215df
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-04-23 23:33:53 +0300

    Add data access layer
    Implement cosmos db repository

---
+++ src/tasks-api/DataAccessLayer/CosmosDbOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace EKids.Chatbot.Tasks.DataAccessLayer;
+
+internal class CosmosDbOptions
+{
+    public required string DatabaseId { get; init; }
+    public required string TasksContainer { get; init; }
+}
---
+++ src/tasks-api/DataAccessLayer/ILearningTaskRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace EKids.Chatbot.Tasks.DataAccessLayer;
+public interface ILearningTaskRepository<ID>
+{
+    IAsyncEnumerable<LearningTask> Save(IEnumerable<LearningTask> tasks, CancellationToken cancellation);
+    Task<LearningTask> FindById(ID id, CancellationToken cancellation);
+    Task DeleteById(ID id, CancellationToken cancellation);
+    IAsyncEnumerable<LearningTask> FindAll(CancellationToken cancellation);
+}
