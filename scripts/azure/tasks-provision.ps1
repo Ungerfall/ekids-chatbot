7c84cdbd616c0ffb2702c1da68036f709712a816
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-05-02 02:45:15 +0300

    Add script for azure cosmos db provision (tasks container)

---
+++ scripts/azure/tasks-provision.ps1
@@ -0,0 +1,47 @@
+$rg = "rg-ekids-chatbot"
+$account = "cosmos-ungerfall-ekids-chatbot"
+$db = "ekids-chatbot"
+$tasksContainerName = "tasks"
+$location = "westeurope"
+
+$loginStatus = az login
+# if the status is null or empty, the login has failed
+if ([string]::IsNullOrEmpty($loginStatus)) {
+    echo "Failed to login. Exiting the script."
+    exit
+}
+
+$resourceGroup = az group exists --name $rg
+if ($resourceGroup -eq "false") {
+    echo "Creating resource group..."
+    az group create --name $rg --location $location
+} else {
+    echo "Resource group already exists."
+}
+
+$cosmosDbAccount = az cosmosdb check-name-exists --name $account
+if ($cosmosDbAccount -eq "false") {
+    echo "Creating Cosmos DB account..."
+    az cosmosdb create --name $account --resource-group $rg --locations regionName=$location failoverPriority=0 isZoneRedundant=False
+} else {
+    echo "Cosmos DB account already exists."
+}
+
+echo "Creating/Ensuring the database exists in the Cosmos DB account..."
+az cosmosdb sql database create --account-name $account --resource-group $rg --name $db
+
+$tasksContainer = az cosmosdb sql container exists --account-name $account `
+                                 --database-name $db `
+                                 --name $tasksContainerName `
+                                 --resource-group $rg
+if ($tasksContainer -eq "false") {
+  echo "Creating the empty $tasksContainerName container..."
+    az cosmosdb sql container create --account-name $account `
+    --database-name $db `
+    --name $tasksContainerName `
+    --partition-key-path "/courseId" `
+    --resource-group $rg `
+    --query "id" `
+} else {
+    echo "Cosmos DB homeworks container already exists."
+}
