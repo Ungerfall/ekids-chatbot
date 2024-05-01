a198d7d23f6bcc10c8f2cc7748e8c90e77198a48
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-05-02 02:47:03 +0300

    Update data access layer
    1. Add cosmos db options
    2. Add cosmos db json serializer (required to properly work with
       System.Text.Json)
    3. Fix queries to use course id partition key

--- src/tasks-api/DataAccessLayer/CosmosDbLearningTaskRepository.cs
+++ src/tasks-api/DataAccessLayer/CosmosDbLearningTaskRepository.cs
@@ -1,16 +1,20 @@
-﻿using System.Runtime.CompilerServices;
-
-using EKids.Chatbot.Tasks.DataAccessLayer.Entities;
-
+﻿using EKids.Chatbot.Tasks.DataAccessLayer.Entities;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
 
 namespace EKids.Chatbot.Tasks.DataAccessLayer;
-internal class CosmosDbLearningTaskRepository(CosmosClient cosmosClient, CosmosDbOptions cosmosDbOptions)
+internal class CosmosDbLearningTaskRepository(
+    CosmosClient cosmosClient,
+    IOptions<CosmosDbOptions> cosmosDbOptions,
+    ILogger<CosmosDbLearningTaskRepository> logger)
     : ILearningTaskRepository
 {
+    private readonly CosmosDbOptions _cosmosDbOptions = cosmosDbOptions.Value;
+
     public async Task DeleteById(Guid courseId, Guid taskId, CancellationToken cancellation)
     {
-        var container = cosmosClient.GetContainer(cosmosDbOptions.DatabaseId, cosmosDbOptions.TasksContainer);
+        var container = cosmosClient.GetContainer(_cosmosDbOptions.DatabaseId, _cosmosDbOptions.TasksContainer);
         await container.DeleteItemAsync<LearningTask>(
             taskId.ToString(),
             new PartitionKey(courseId.ToString()),
@@ -18,16 +22,19 @@ internal class CosmosDbLearningTaskRepository(CosmosClient cosmosClient, CosmosD
             cancellation);
     }
 
-    public async IAsyncEnumerable<LearningTask> FindAll([EnumeratorCancellation] CancellationToken cancellation)
+    public async IAsyncEnumerable<LearningTask> FindAll(Guid? courseId, [EnumeratorCancellation] CancellationToken cancellation)
     {
-        var container = cosmosClient.GetContainer(cosmosDbOptions.DatabaseId, cosmosDbOptions.TasksContainer);
-        var query = new QueryDefinition("SELECT * FROM c");
+        var container = cosmosClient.GetContainer(_cosmosDbOptions.DatabaseId, _cosmosDbOptions.TasksContainer);
+        var query = courseId is null
+            ? new QueryDefinition("SELECT * FROM c")
+            : new QueryDefinition("SELECT * FROM c WHERE c.courseId = @courseId")
+                .WithParameter("@courseId", courseId.Value.ToString());
         using var it = container.GetItemQueryIterator<LearningTask>(
             query,
             requestOptions: new QueryRequestOptions
             {
                 MaxConcurrency = 1,
-                PartitionKey = PartitionKey.None,
+                PartitionKey = courseId is null ? null : new PartitionKey(courseId.Value.ToString()),
             });
         while (it.HasMoreResults)
         {
@@ -37,24 +44,37 @@ internal class CosmosDbLearningTaskRepository(CosmosClient cosmosClient, CosmosD
                 cancellation.ThrowIfCancellationRequested();
                 yield return item;
             }
+
+            if (response.Diagnostics != null)
+            {
+                logger.LogWarning("Diagnostics: {diagnostics}", response.Diagnostics.ToString());
+            }
         }
     }
 
-    public async Task<LearningTask> Find(Guid courseId, Guid taskId, CancellationToken cancellation)
+    public async Task<LearningTask?> Find(Guid courseId, Guid taskId, CancellationToken cancellation)
     {
-        var container = cosmosClient.GetContainer(cosmosDbOptions.DatabaseId, cosmosDbOptions.TasksContainer);
-        return await container.ReadItemAsync<LearningTask>(
-            taskId.ToString(),
-            new PartitionKey(courseId.ToString()),
-            cancellationToken: cancellation);
+        var container = cosmosClient.GetContainer(_cosmosDbOptions.DatabaseId, _cosmosDbOptions.TasksContainer);
+        try
+        {
+            return await container.ReadItemAsync<LearningTask>(
+                taskId.ToString(),
+                new PartitionKey(courseId.ToString()),
+                cancellationToken: cancellation);
+        }
+        catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            logger.LogWarning(ex, "Task has not been found. Returning null");
+            return null;
+        }
     }
 
-    public async IAsyncEnumerable<LearningTask> Save(IEnumerable<LearningTask> tasks, [EnumeratorCancellation] CancellationToken cancellation)
+    public async Task Save(IEnumerable<LearningTask> tasks, CancellationToken cancellation)
     {
-        var container = cosmosClient.GetContainer(cosmosDbOptions.DatabaseId, cosmosDbOptions.TasksContainer);
+        var container = cosmosClient.GetContainer(_cosmosDbOptions.DatabaseId, _cosmosDbOptions.TasksContainer);
         foreach (var task in tasks)
         {
-            yield return await container.UpsertItemAsync(
+            await container.UpsertItemAsync(
                 task,
                 new PartitionKey(task.CourseId),
                 new ItemRequestOptions { EnableContentResponseOnWrite = true },
--- src/tasks-api/DataAccessLayer/TasksJsonSerializerContext.cs
+++ src/tasks-api/DataAccessLayer/TasksJsonSerializerContext.cs
@@ -1,9 +1,10 @@
-﻿using System.Text.Json.Serialization;
-
-using EKids.Chatbot.Tasks.DataAccessLayer.Entities;
+﻿using EKids.Chatbot.Tasks.DataAccessLayer.Entities;
+using System.Text.Json.Serialization;
 
 namespace EKids.Chatbot.Tasks.DataAccessLayer;
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
 [JsonSerializable(typeof(LearningTask))]
+[JsonSerializable(typeof(IAsyncEnumerable<LearningTask>))]
 [JsonSerializable(typeof(LearningTask[]))]
 [JsonSerializable(typeof(TaskComplexity))]
 internal partial class TasksJsonSerializerContext : JsonSerializerContext;
