7a3fd8651c002303c9b526133569707cb68b4446
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-05-02 02:51:24 +0300

    Register required services

--- src/tasks-api/Program.cs
+++ src/tasks-api/Program.cs
@@ -1,5 +1,8 @@
+using EKids.Chatbot.Tasks.BusinessLayer;
 using EKids.Chatbot.Tasks.DataAccessLayer;
 using EKids.Chatbot.Tasks.PresentationLayer;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Options;
 
 namespace EKids.Chatbot.Tasks;
 
@@ -11,6 +14,21 @@ internal static class Program
 
         builder.Services.ConfigureHttpJsonOptions(options
             => options.SerializerOptions.TypeInfoResolverChain.Insert(0, TasksJsonSerializerContext.Default));
+        builder.Services.AddOptions<CosmosDbOptions>()
+            .Bind(builder.Configuration.GetSection(CosmosDbOptions.ConfigSection));
+        builder.Services.AddSingleton(sp =>
+        {
+            var opt = sp.GetRequiredService<IOptions<CosmosDbOptions>>().Value;
+            return new CosmosClient(
+                opt.CosmosDbConnectionString,
+                clientOptions: new CosmosClientOptions
+                {
+                    MaxRetryAttemptsOnRateLimitedRequests = 3,
+                    Serializer = new CosmosSystemTextJsonSerializer(),
+                });
+        });
+        builder.Services.AddScoped<ILearningTaskRepository, CosmosDbLearningTaskRepository>();
+        builder.Services.AddScoped<ILearningTaskService, LearningTaskService>();
 
         var app = builder.Build();
         app.MapLearningTasks();
