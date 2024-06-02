9542ddc12c214d975c3c1e96885f90ce2489c82c
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-06-03 01:26:54 +0300

    Add infrastructure layer

---
+++ src/homeworks-api/src/EKids.Chatbot.Homeworks.Infrastructure/Data/HomeworkContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace EKids.Chatbot.Homeworks.Infrastructure.Data;
+
+public class HomeworkContextSeed
+{
+    public static async Task SeedAsync(HomeworkContext homeworkContext, ILoggerFactory loggerFactory, int? retry = 0)
+    {
+        int retryForAvailability = retry ?? 0;
+
+        try
+        {
+            // TODO: Only run this if using a real database
+            homeworkContext.Database.Migrate();
+            homeworkContext.Database.EnsureCreated();
+        }
+        catch (Exception exception)
+        {
+            if (retryForAvailability < 10)
+            {
+                retryForAvailability++;
+                var log = loggerFactory.CreateLogger<HomeworkContextSeed>();
+                log.LogError("Exception {Message}", exception.Message);
+                await SeedAsync(homeworkContext, loggerFactory, retryForAvailability);
+            }
+
+            throw;
+        }
+    }
+}
---
+++ src/homeworks-api/src/EKids.Chatbot.Homeworks.Infrastructure/Exceptions/InfrastructureException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EKids.Chatbot.Homeworks.Infrastructure.Exceptions;
+
+public class InfrastructureException : Exception
+{
+    internal InfrastructureException(string businessMessage)
+           : base(businessMessage)
+    {
+    }
+
+    internal InfrastructureException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
