76871b9fecde124c4f850069757c2f95162bf22d
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-06-03 01:26:54 +0300

    Add infrastructure layer

---
+++ src/homeworks-api/src/EKids.Chatbot.Homeworks.Infrastructure/Repository/HomeworkRepository.cs
@@ -0,0 +1,9 @@
+﻿using EKids.Chatbot.Homeworks.Core.Entities;
+using EKids.Chatbot.Homeworks.Infrastructure.Data;
+using EKids.Chatbot.Homeworks.Infrastructure.Repository.Base;
+
+namespace EKids.Chatbot.Homeworks.Infrastructure.Repository;
+
+public class HomeworkRepository(HomeworkContext dbContext) : Repository<Homework>(dbContext)
+{
+}
