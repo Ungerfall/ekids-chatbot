c9b1e197c9f2d96ba1ae7f1f1a95dd41b7c2b493
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-08-05 05:41:53 +0300

    Setup Aspire projects with chatbot and users api

--- src/orchestration/EKids.Chatbot.AppHost/Program.cs
+++ src/orchestration/EKids.Chatbot.AppHost/Program.cs
@@ -1,3 +1,10 @@
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var usersService = builder.AddProject<Projects.EKids_Chatbot_Users_WebApi>("users-api");
+
+builder.AddProject<Projects.EKids_Chatbot_Telegram_Worker>("chatbot-worker")
+    .WithExternalHttpEndpoints()
+    .WithReference(usersService);
 
 builder.Build().Run();
