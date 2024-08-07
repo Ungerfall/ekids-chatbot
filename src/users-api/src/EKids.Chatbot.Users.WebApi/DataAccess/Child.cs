5edcedf2a159eea00c7b79edbe0819a7822dd687
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-08-07 08:10:37 +0300

    Add children db set

---
+++ src/users-api/src/EKids.Chatbot.Users.WebApi/DataAccess/Child.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EKids.Chatbot.Users.WebApi.DataAccess;
+public class Child
+{
+    public Guid Id { get; set; }
+
+    public Guid ChildUserId { get; set; }
+    public IdentityUser<Guid> ChildUser { get; set; } = null!;
+
+    public Guid ParentUserId { get; set; }
+    public IdentityUser<Guid> ParentUser { get; set; } = null!;
+}
--- src/users-api/src/EKids.Chatbot.Users.WebApi/DataAccess/UsersDbContext.cs
+++ src/users-api/src/EKids.Chatbot.Users.WebApi/DataAccess/UsersDbContext.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using EKids.Chatbot.Users.WebApi.DataAccess;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EKids.Chatbot.Users.DataAccess;
 public sealed class UsersDbContext(DbContextOptions<UsersDbContext> options) : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>(options)
 {
+    public DbSet<Child> Children { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Child>(e =>
+        {
+            e.ToTable("Children");
+            e.HasKey(x => x.Id);
+            e.HasOne(x => x.ParentUser)
+                .WithMany()
+                .HasForeignKey(x => x.ParentUserId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+            e.HasOne(x => x.ChildUser)
+                .WithOne()
+                .HasForeignKey<Child>(x => x.ChildUserId)
+                .OnDelete(DeleteBehavior.NoAction)
+                .IsRequired();
+        });
+    }
 }
