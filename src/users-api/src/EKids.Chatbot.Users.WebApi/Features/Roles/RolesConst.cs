91a9d1b0b4a7022face21f1a8522795a9fb3c490
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-08-07 08:12:17 +0300

    Add roles

---
+++ src/users-api/src/EKids.Chatbot.Users.WebApi/Features/Roles/RolesConst.cs
@@ -0,0 +1,7 @@
+﻿namespace EKids.Chatbot.Users.WebApi.Features.Roles;
+public static class RolesConst
+{
+    public const string Administrator = "Administrator";
+    public const string Parent = "Parent";
+    public const string Child = "Child";
+}
